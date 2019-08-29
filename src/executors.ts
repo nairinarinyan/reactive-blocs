297ff8a8986b30fad2b91f2e46e6eacd1a40b42d
Nairi Narinyan <nairi.narinyan@gmail.com>
2019-08-29 15:03:49 +0400

    add ExecSubject and generic executor

--- src/executors.ts
+++ src/executors.ts
@@ -1,5 +1,5 @@
-import { Observable, BehaviorSubject, of } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { Observable, BehaviorSubject, of, Subject, merge, Subscriber, Subscription, SubscriptionLike, throwError } from 'rxjs';
+import { distinctUntilChanged, mergeMap, catchError, share, switchMap, filter, tap } from 'rxjs/operators';
 
 export type Executor<T> = (...execArgs: any[]) => Observable<T>;
 export type ExecPerformer = (...execArgs: any[]) => void;
@@ -42,4 +42,44 @@ export function execControlled<T>(exec: Executor<T>, control: Control, initialVa
     executeImmediately && doExec();
 
     return subject;
+}
+
+export class ExecSubject<T> extends BehaviorSubject<T> {
+    private _executionStream = new Subject<any>();
+
+    constructor(
+        private _executor: Executor<T>,
+        _initialValue?: T,
+        private _error ?: BehaviorSubject<Error>,
+    ) {
+        super(_initialValue);
+        this.init();
+    }
+
+    private init() {
+        this._executionStream.pipe(
+            switchMap(args => this.performExec(this._executor, args)),
+            tap(error => {
+                if (error instanceof Error && this._error) {
+                    this._error.next(error);
+                }
+            }),
+            filter(result => !(result instanceof Error)),
+            share(),
+        ).subscribe(val => this.next(val as T));
+    }
+
+    private performExec<T>(exec: Executor<T>, args: any) { 
+        return exec(args).pipe(
+            catchError(err => of(new Error(err)))
+        );
+    };
+
+    exec(args: any) {
+        this._executionStream.next(args);
+    }
+}
+
+export function exec<T>(exec: Executor<T>, initialValue?: T, error?: BehaviorSubject<Error>): ExecSubject<T> {
+    return new ExecSubject<T>(exec, initialValue, error);
 }
