676d9037d71c32a2ca23ef7019465f3a69159ddc
Nairi Narinyan <nairi.narinyan@gmail.com>
2019-12-06 18:57:13 +0400

    generic actions

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,10 @@
 import { Subject, Observable, isObservable } from 'rxjs';
 import { filter, mergeMap, map } from 'rxjs/operators';
 
-export interface Action {
-    id: number;
+export interface Action<T = any> {
+    id?: number;
     type: string;
-    args: any[];
+    args: T;
 }
 
 export interface ActionResult {
@@ -12,18 +12,18 @@ export interface ActionResult {
     result: any;
 }
 
-type ActionHandler = (...args: any[]) => any;
+type ActionHandler<T = any> = (args: T) => any;
 
 const actionStream = new Subject<Action>();
 const responseStream = new Subject<ActionResult>();
 
-export const actionOf = (type: string, handler: ActionHandler) => {
+export const actionOf = <T = any>(type: string, handler: ActionHandler<T>) => {
     return actionStream
         .pipe<Action, ActionResult>(
             filter(action => action.type === type),
             mergeMap(action => {
                 const { id, args } = action;
-                const response = handler(...args);
+                const response = handler(args);
 
                 return new Observable<ActionResult>(obs => {
                     if (isObservable(response)) {
@@ -40,7 +40,7 @@ export const actionOf = (type: string, handler: ActionHandler) => {
         .subscribe(result => responseStream.next(result));
 };
 
-export const dispatch = (type: string, ...args: any[]) => {
+export const dispatch = <T = any>(type: string, args?: T) => {
     const action: Action = {
         id: Math.random() * (100000 - 100) + 100 << 0,
         type, args
